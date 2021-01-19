7f3829461b6ac585a7f334080738ee6be652b462
MartinTam <hongvtam@protonmail.com>
2021-01-19 12:40:25 +0100

    Add field to function

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,40 +1,27 @@
 
-for first in range(26):
-    if first==0:
-        print(' ', end='')
-    print('_', end='')
-
-print()
-
-for row in range(3):
-    for col in range(0,28):
-        if col == 0 or col%9==0:
-            print('|', end='')
-        elif row==2:
-            print('_', end='')
-        else:
+def firstRow():
+    for first in range(26):
+        if first==0:
             print(' ', end='')
+        print('_', end='')
+
     print()
 
-for row in range(3):
-    for col in range(0,28):
-        if col == 0 or col%9==0:
-            print('|', end='')
-        elif row==2:
-            print('_', end='')
-        else:
-            print(' ', end='')
-    print()
+def otherRow():
 
-for row in range(3):
-    for col in range(0,28):
-        if col == 0 or col%9==0:
-            print('|', end='')
-        elif row==2:
-            print('_', end='')
-        else:
-            print(' ', end='')
-    print()
+    for row in range(3):
+        for col in range(28):
+            if col == 0 or col%9==0:
+                print('|', end='')
+            elif row==2:
+                print('_', end='')
+            else:
+                print(' ', end='')
+        print()
 
-print()
+firstRow()
+for i in range(3):
+    otherRow()
+print()    
+
 
